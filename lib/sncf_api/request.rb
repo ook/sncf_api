9a7008e75ec892c020034b6feda1c6fe605c4290
Thomas Lecavelier <thomas@followanalytics.com>
2015-06-17 07:09:06 +0200

    Non passing stuff

--- lib/sncf_api/request.rb
+++ lib/sncf_api/request.rb
@@ -8,12 +8,43 @@ module SncfApi
         api_token = nil if !api_token.is_a?(String) || api_token.strip == ''
         raise ArgumentError, "You MUST specify either api_token argument nor SNCF_API_TOKEN env variable" unless api_token
         @instances ||= {}
-        @instances[api_token] ||= new
+        @instances[api_token] ||= new(plan: plan[:limits])
       end
     end
 
+    # My understanding of SNCF quota is that the countdown start at the very first call of a dau and the plan start on that instant
+    def countdown
+      @countdow ||= default_countdown  
+    end
+
   private
-    def initialize
+    def initialize(plan:)
+      @plan = plan
+    end
+
+    def default_countdown
+      { per_day: @plan[:per_day], per_month: @plan[:per_month], started_at: now }
+    end
+
+    def now
+      Time.now.utc
+    end
+
+    QUOTA_PERIODS = { 
+                      per_day:        24 * 60 * 60,
+                      per_month: 30 * 24 * 60 * 60
+                     }
+    def decrease_quotas
+      @countdown ||= default_countdown
+      right_now = now
+      QUOTA_PERIODS.each do |key, period|
+        if @countdown[:started_at] > (right_now - period)
+          @countdown[key] = @plan[key]
+          @countdown[:started_at] = right_now
+        end
+      end
+      QUOTA_PERIODS.keys.each { |k| @countdown[k] -= 1 }
+      @countdown
     end
   end
 end
--- spec/lib/sncf_api/request_spec.rb
+++ spec/lib/sncf_api/request_spec.rb
@@ -18,4 +18,48 @@ describe SncfApi::Request do
       end
     end
   end
+
+  describe '#countdown' do
+    context 'with a free account' do
+      let(:request) { SncfApi::Request.instance(api_token: 'fakeToken') }
+
+      let!(:frozen_now) { Time.new(2009,5,16, 14).utc }
+      let!(:default_countdown) { { per_day: 3_000, per_month: 90_000, started_at: frozen_now } }
+
+      before(:each) do
+        allow_any_instance_of(Time).to receive_message_chain(:now, :utc).and_return(frozen_now)
+        allow_any_instance_of(SncfApi::Request).to receive(:now).and_return(frozen_now)
+      end
+
+      it 'should have a stock of 3 000 calls per day and 90K per month' do
+        expect(request.countdown).to eq(default_countdown)
+      end
+
+      it 'should decrease countdown after each successful callahave a stock of 3 000 calls per day and 90K per month' do
+        cd_after_a_request = default_countdown
+        cd_after_a_request[:per_day] -= 1
+        cd_after_a_request[:per_month] -= 1
+        request.send(:decrease_quotas)
+        expect(request.countdown).to eq(cd_after_a_request)
+      end
+
+      it 'should reset day countdown after exceed the period time without touching month countdown' do
+        before_request_count = 1_000
+        before_request_count.times { request.send(:decrease_quotas) }
+        cd_after_a_request = default_countdown
+        cd_after_a_request[:per_day] -= before_request_count
+        cd_after_a_request[:per_month] -= before_request_count
+        expect(request.countdown).to eq(cd_after_a_request)
+
+        the_day_after = frozen_now + 1.day
+        allow_any_instance_of(Time).to receive_message_chain(:now, :utc).and_return(the_day_after)
+        allow_any_instance_of(SncfApi::Request).to receive(:now).and_return(the_day_after)
+
+        cd_after_a_request[:per_day] = default_countdown[:per_day] -1
+        cd_after_a_request[:per_month] -= 1
+        request.send(:decrease_quotas)
+        expect(request.countdown).to eq(cd_after_a_request)
+      end
+    end
+  end
 end
