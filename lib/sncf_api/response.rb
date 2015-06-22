67f303d4a426732423c1ea2750b2d08ed5c2f8cc
Thomas Lecavelier <thomas@followanalytics.com>
2015-06-23 00:04:12 +0200

    Implements #each_page

--- lib/sncf_api/response.rb
+++ lib/sncf_api/response.rb
@@ -1,14 +1,20 @@
 require 'http'
 require 'oj'
 module SncfApi
+  # A reponse represent a request response from API. Note that means only ONE PAGE AT A TIME
+  # There's helper methods like #each_page and #page(index) to help you to crawl easily in the 
+  # result
   class Response
     BASE_URL = 'https://api.sncf.com/v1'
 
-    attr_reader :response, :request, :body, :content
+    attr_reader :response, :request, :body, :content, :http_params
+    attr_reader :pagination, :start_page
 
     KNOWN_HTTP_ERROR_CODES = [400, 401, 404, 500]
-    def initialize(path:, request:)
-      @response = Http.basic_auth(:user => request.api_token, :pass => nil).get(BASE_URL + path)
+    def initialize(path:, request:, http_params: nil)
+      @path = path
+      @http_params = http_params
+      @response = Http.basic_auth(:user => request.api_token, :pass => nil).get(BASE_URL + @path, params: @http_params)
       @body = response.body
       @request = request
       @request.send :decrease_quotas
@@ -20,10 +26,47 @@ module SncfApi
           break if chunk.nil?
           @content << chunk
         end
-        @content = Oj.load(@content) if @response.content_type.mime_type == 'application/json'
+        if @response.content_type.mime_type == 'application/json'
+          @content = Oj.load(@content) 
+          @pagination = @content['pagination']
+          @start_page = @pagination ? @pagination['start_page'] : 0
+        end
       end
       if KNOWN_HTTP_ERROR_CODES.include?(@response.code)
         raise ArgumentError, "Unauthorized (#{@response.code}) #{@response.body}"
+      end
+    end
+
+    # return Hash
+    def links
+      @content['links']
+    end
+
+    def page(index=0)
+      new_http_params = @http_params.dup
+      new_http_params['start_page'] = index
+      SncfApi::Response.new(path: @path, request: @request, http_params: new_http_params)
+    end
+
+    # Execute given block from the current page until last rel=next
+    def each_page(&block)
+      yield @content
+      content = @content
+      loop do
+        next_page = content && content['links'] && content['links'].find { |e| e['type'] == 'next' }
+        break unless next_page
+        resp = Http.basic_auth(user: request.api_token, pass: nil).get(next_page['href'])
+        @request.send :decrease_quotas
+        break unless resp.code == 200 
+        break unless resp.content_type.mime_type == 'application/json'
+        content = ''
+        loop do
+          chunk = resp.body.readpartial(HTTP::Connection::BUFFER_SIZE) rescue nil
+          break if chunk.nil?
+          content << chunk
+        end
+        content = Oj.load(content)
+        yield content
       end
     end
 
--- spec/lib/sncf_api/response_spec.rb
+++ spec/lib/sncf_api/response_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe SncfApi::Response do
-  describe 'content' do
+  describe '#content' do
     let(:response) { SncfApi::Response.new(path: '/fake-path', request: SncfApi::Request.instance(api_token: 'fake-token')) }
 
     [400,401,404, 500].each do |code|
@@ -19,6 +19,18 @@ describe SncfApi::Response do
       writer.close
       allow(Http).to receive_message_chain(:basic_auth, :get).and_return(Http::Response.new 200, nil, {'Content-Type' => 'application/json'}, reader)
       expect(SncfApi::Response.new(path: '/coverage', request: SncfApi::Request.instance(api_token: 'fake-token')).content).to eq(expected)
+    end
+  end
+
+  describe '#links which expose HATEOAS links. The keys are the rel' do
+    it 'expose rel as keys and links as values' do
+      fixture_pathname = Pathname(__FILE__) + '..' + '..' + '..' + 'fixtures' + 'coverage_page1.json'
+      expected = Oj.load(fixture_pathname.read)['links']
+      reader, writer  = IO.pipe
+      writer << fixture_pathname.read << "\n"
+      writer.close
+      allow(Http).to receive_message_chain(:basic_auth, :get).and_return(Http::Response.new 200, nil, {'Content-Type' => 'application/json'}, reader)
+      expect(SncfApi::Response.new(path: '/coverage', request: SncfApi::Request.instance(api_token: 'fake-token')).links).to eq(expected)
     end
   end
 end
